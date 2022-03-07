fa7e1a58305f37ed0affff0b6cbabbbb69d094e1
angjaay <angga.angzai@gmail.com>
2022-03-07 20:21:00 +0700

    adding latest project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@ module.exports = {
   content: ['./index.html'],
   darkMode:'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.7s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      },
+      variants: {
+        animation: ["motion-safe"]
+    },
+    backgroundImage: {
+      'hero-pattern': "url('Assets/images/bg.png')",
+      'liquid': "url('Assets/images/liquid.png')"
+      
+    }
+    },
   },
   plugins: [],
 }
